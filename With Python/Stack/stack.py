fa277c49d30481e6f3e8f2259a722882fe87ddc6
honggom <cavok699@naver.com>
2021-05-28 17:32:05 +0900

    Queue

--- With Python/Stack/stack.py
+++ With Python/Stack/stack.py
@@ -8,13 +8,13 @@ class Stack():
     def push(self, item):
         self.stack.append(item)
 
-    # 맨 위 데이터 리턴 및 삭제
+    # 맨 위(마지막) 데이터 리턴 및 삭제
     def pop(self):
         if self.isEmpty():
             return 'empty..'
         return self.stack.pop()
 
-    # 맨 위 데이터 리턴
+    # 맨 위(마지막) 데이터 리턴
     def peek(self):
         if self.isEmpty():
             return 'empty..'
@@ -25,3 +25,12 @@ class Stack():
         if len(self.stack) == 0:
             return True
         return False
+
+
+s = Stack()
+s.push(1)
+s.push(2)
+s.push(3)
+
+print(s)
+print(s.pop())
